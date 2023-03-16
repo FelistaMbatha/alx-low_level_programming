7830d8adbe26daa18bba38dd6a285bef03ca7e5f
FelistaMbatha <mbathafelista@gmail.com>
2023-03-16 14:55:04 -0700

    Funny commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints specific lines with the puts function
- * Return: 0
+ * Return : 0
  */
 int main(void)
-(
-puts(\"Programming is like building a multilingual puzzle"
-);
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-) 
+} 
